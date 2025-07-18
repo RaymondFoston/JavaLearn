8d55d6034ed344694dc8321982feda042997a66c
RaymondFoston <bdlt368@gmail.com>
2025-07-18 12:34:02 +0800

    import packages

--- SpringMVC/src/main/java/com/company/springmvc/cortoller/FouthController.java
+++ SpringMVC/src/main/java/com/company/springmvc/cortoller/FouthController.java
@@ -1,6 +1,10 @@
 package com.company.springmvc.cortoller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FouthController {
